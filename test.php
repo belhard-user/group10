38418bb13a8f5caf4091481354e24d9ca2b95fe3
belhard-user <ldarik2l@gmail.com>
2016-12-18 13:13:44 +0300

    PDO

--- test.php
+++ test.php
@@ -1,20 +1,37 @@
 <?php
 
 require "vendor/autoload.php";
-use Acme\Foo;
 
-foo();
-bar();
 
-new User();
-new UserRepository();
-new Git();
+class Peoples
+{
 
-new \Acme\User\Foo();
+    public function getEmail()
+    {
+        return strtoupper($this->email);
+    }
+}
 
-$names = [
-    'boss' => 'Morpheus',
-    'the one' => 'Neo'
-];
 
-dump([1, 2, 3, 4], new Foo(), $names);
+try {
+     $pdo = new PDO('mysql:dbname=smart;host=localhost', 'root', '', [
+         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+     ]);
+
+     // $sql = "DELETE FROM test WHERE id IN (1, 3, 5)";
+     // $result = $pdo->exec($sql);
+
+    $result = $pdo->query('SELECT * FROM test');
+
+    foreach ($result->fetchAll() as $item){
+        echo $item->email . '<hr>';
+    }
+
+     /*while($item = $result->fetch(PDO::FETCH_OBJ)){
+         dump($item);
+     }*/
+
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
